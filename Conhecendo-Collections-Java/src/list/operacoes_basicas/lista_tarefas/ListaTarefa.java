fa8f1df572d338ccd70b4cb75108673083a1cdae
IcaroRaphael <icaro2551@gmail.com>
2023-09-08 19:56:39 -0300

    feat: Operações Básicas com List - Lista de Tarefas

---
+++ Conhecendo-Collections-Java/src/list/operacoes_basicas/lista_tarefas/ListaTarefa.java
@@ -0,0 +1,47 @@
+package list.operacoes_basicas.lista_tarefas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefas;
+
+    public ListaTarefa() {
+        tarefas = new ArrayList<>();
+    }
+
+    public List<Tarefa> getTarefas() {
+        return tarefas;
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefas.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        if(!tarefas.isEmpty()){
+            for(Tarefa t : tarefas){
+                if(t.getDescricao().equalsIgnoreCase(descricao)){
+                    tarefas.remove(t);
+                    break;
+                }
+            }
+        } else{
+            System.out.println("Lista vazia!");
+        }
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return tarefas.size();
+    }
+
+    public void obterDescricoesTarefas(){
+        if(!tarefas.isEmpty()){
+            for(Tarefa t : tarefas){
+                System.out.println(t.getDescricao());
+            }
+        } else{
+            System.out.println("Lista vazia!");
+        }
+    }
+}
